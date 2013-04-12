52a039bb3480732d41e9d239bf9fd3c74431eb10
Rajan <rajanlucky001@gmail.com>
2013-04-13 02:29:43 +0530

    Use Case Diagram

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApplicationDeploymentManager::Application.config.secret_token = 'cb7a7ad755f37a8173092c6b80b19d929139e3bcb58f73bdf21e2f6c16cd19edc0123412390565cd92a189a2a4ea03d3787dd8593a2ca5bdaa152b3726c2af81'
